edb7f70886c3031ed06f8ec9539292e623457041
KirTsarev <kirtsarev@gmail.com>
2026-02-06 21:44:33 +0200

    config: update Vite configuration settings.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     server: {
+        // 👇 Добавлена строка для разрешения вашего хоста
+        allowedHosts: ['dymacademy.xyz'],
         port: 5173,
         proxy: {
             '/api': {
